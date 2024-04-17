58a3408200f1ccb00decbe79f7e1875049f524c3
ClarkGodwin <127951623+ClarkGodwin@users.noreply.github.com>
2024-04-17 20:42:56 +0200

    main.c

--- main.c
+++ main.c
@@ -8,9 +8,7 @@ void lh()
 int main()
 {
     srand(time(NULL));
-    unsigned int nbr1=rand()%100, nbr2=rand()%200,nbrf=0,N,i=1,u;
-    char rep;
-    printf("%u",nbr1);
+    unsigned int nbr1=rand()%100, nbr2=rand()%200,nbrf=0,N,i=1,u,rep;
     lh();
     printf("\t         GUESSING GAME        \n");
     lh();
@@ -18,64 +16,63 @@ int main()
     lh();
     printf("\t     BIENVENU AU  NIVEAU 1    \n");
     lh();
-    printf(" Un nombre au hasard a ete choisi, devinez-le en au plus 6 coups: \n");
+    printf(" Un nombre au hasard a ete choisi, devinez-le en au plus 6 coups: ");
     nbrf=6;
     do
     {
         scanf("%u",&N);
         if(N == nbr1)
         {
-            printf("BRAVO VOUS AVEZ REUSSSI EN %u COUP(s)\n",i);
+            printf("\n BRAVO VOUS AVEZ REUSSSI EN %u COUP(s)\n",i);
             break;
         }
         else if (N < nbr1)
         {
-            printf(" TROP PETIT\n");
+            printf("\n Plus grand que ca, il vous reste %i coup(s)", 6-i);
         }
         else if (N > nbr1)
         {
-            printf(" TROP GRAND\n");
+            printf("\n Plus p'tit que ca, il vous reste %i coup(s)", 6-i);
         }
         if(i == 6 && N != nbr1)
         {
-            printf("VOUS AVEZ PERDU\n\n");
+            printf("\n Vous avez perdu\n\n");
         }
         i++;
     }while (i <= 6);
     if (i <= 4)
     {
         u=1;
-        printf("Voulez-vous passer au niveau suivant(o\\n): ");
-        scanf("%c", &rep);
-        if (rep == 'o' || rep == 'O')
+        printf("\n Voulez-vous passer au niveau suivant(1 pour oui, 0 pour non): ");
+        scanf("%i", &rep);
+        if (rep == 1)
         {
             lh();
             printf("\t     BIENVENU AU  NIVEAU 2    \n");
             lh();
-            printf("Cette fois ci, devinez ce nombre en au plus 4 coups: \n");
-            nbrf=4;
+            printf("\n Cette fois ci, devinez ce nombre en au plus 10 coups: ");
                 do
                 {
                     scanf("%u",&N);
                     if(N == nbr2)
                     {
-                        printf("BRAVO VOUS AVEZ REUSSSI EN %u COUP(s)\n",i);
+                        printf("BRAVO VOUS AVEZ REUSSSI EN %u COUP(s)\n", 10-u);
                         break;
                     }
                     else if (N < nbr2)
                     {
-                        printf(" TROP PETIT\n");
+                        printf("\n Plus grand que ca, il vous reste %i coup(s)", 10-u);
                     }
                     else if (N > nbr2)
                     {
-                        printf(" TROP GRAND\n");
+                        printf("\n Plus petit que ca, il vous reste %i coup(s)", 10-u);
                     }
-                    if(u == 4 && N != nbr2)
+                    if(u == 10 && N != nbr2)
                     {
-                        printf("VOUS AVEZ PERDU\n\n");
+                        printf("\n Vous avez perdu\n\n");
                     }
-                    i++;
-            }while (u <= 4);
+                    u++;
+            }while (u <= 10);
         }
     }
     else
